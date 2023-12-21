d170cd28b32ef12d9fe9727370fcf817b993c6dc
Petr Zajkas <petrzajkas@gmail.com>
2023-12-21 03:18:50 +0000

    single product

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -38,6 +38,8 @@ export default function ImageGallery ({ images }: iAppProps) {
                         height={500} 
                         className="h-full w-full object-cover object-center" 
                         />
+
+                        <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 test-sm uppercase tracking-wider text-white">sale</span>
                 </div>
             </div>
         )
